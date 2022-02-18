4f680d289d666823ab21b069e00f3feea69197c3
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-18 15:15:08 +0530

    [add] Shubham added UC5 of EmpPayrollApp-HomePage program

--- js/new_home.js
+++ js/new_home.js
@@ -1,7 +1,8 @@
 /* UC4: Display Employee Details in Tabular Format using Template Literals */
 
 window.addEventListener('DOMContentLoaded', () => {
-    createInnerHTML();
+    //createInnerHTML();
+    createInnerHTMLUC5();
 });
 
 // Template literal ES6 feature
@@ -23,4 +24,71 @@ const createInnerHTML = () => {
         </tr>
         `;
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+/* UC5:- Ability to view Employee Payroll details in a Tabular Format from JSON Object. */
+const createInnerHTMLUC5 = () => {
+    const headerHtml="<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>"
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for (const empPayrollData of empPayrollList) {
+      innerHtml = `${innerHtml}
+      <tr>
+        <td><img class="profile" alt="" 
+                  src="${empPayrollData._profilePic}">
+        </td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${empPayrollData._startDate}</td>
+        <td>
+          <img name="${empPayrollData._id}" onclick="remove(this)" 
+               src="../assets/icons/delete-black-18dp.svg" alt="delete">
+          <img name="${empPayrollData._id}" onclick="update(this)" 
+               src="../assets/icons/create-black-18dp.svg" alt="edit">
+        </td>
+      </tr>
+      `;
+    }
+    document.querySelector("#table-display").innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Shubham Seth',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '29 Oct 2020',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -3.png'
+        },
+        {
+            _name: 'Anjali Seth',
+            _gender: 'female',
+            _department: [
+                'Sales',
+            ],
+            _salary: '400000',
+            _startDate: '29 March 2019',
+            _note: '',
+            _id: new Date().getTime() + 1,
+            _profilePic: '../assets/profile-images/Ellipse -1.png'
+        }
+    ];
+    return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList) {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
 }
